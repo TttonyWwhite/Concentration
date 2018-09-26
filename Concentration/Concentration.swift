486dce7f0373b2cb424fc13f98516d6c98974b04
illiant <effortblesses@outlook.com>
2018-09-26 15:57:47 +0800

    playable version

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,20 +12,49 @@ class Concentration
 {
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     init(numberOfPairsOfCards: Int) {
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
         }
+        cards = shuffleArray(arr: cards)
     }
     
-    // TODO Shuffle the cards
+    
     
     func chooseCard (at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipIndex in cards.indices {
+                    cards[flipIndex].isFaceUp = false
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
+    }
+    
+    func shuffleArray(arr: [Card]) -> [Card] {
+        var data: [Card] = arr
+        for i in 1..<arr.count {
+            let index: Int = Int(arc4random()) % i
+            if index != i {
+                data.swapAt(i, index)
+            }
+        }
+        
+        return data
     }
 }
